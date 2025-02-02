85ca939e63fe0fa9802599f73d7a5be743813abd
ssalha75 <ssalha@unomaha.edu>
2025-02-02 23:36:22 +0000

    Done with FutureTime.

--- FutureTime.py
+++ FutureTime.py
@@ -8,40 +8,37 @@ import datetime
 
 def main():
   #getting current time from system, storing to variable
-
   now = datetime.datetime.now()
-  currentHour = (now.hour - 6)
+  currentHour = (now.hour - 6) % 24
   currentMinute = now.minute
 
   print (currentHour, currentMinute) #this is just for checking, we should delete it later
 
   #TODO:
   #Ask user for hours
-
   hours = input("Enter hours: ")
   hours = int(hours) #change from text to number
   futureHour = currentHour + hours
-  futureHour = futureHour % 24
 
   #Ask user for minutes
-
   minutes = input("Enter minutes: ")
   minutes = int(minutes)
   futureMinute = currentMinute + minutes
+
+  #For minutes that spill over into the next hour
+  extraHours = futureMinute // 60
   futureMinute = futureMinute % 60
+  futureHour = (futureHour + extraHours) % 24
 
   #Calculate the time after the user-supplied time has passed.
-
   strHour = str(futureHour)
   strMinute = str(futureMinute)
 
   #Do not use any if statements in calculating the time.
- 
   if futureMinute < 10:
     strMinute = "0" + strMinute #add a leading zero to one digit minute
 
   #Output the future time in standard format "HH:MM"
- 
   print(strHour + ":" + strMinute)
 
 if __name__ == '__main__':
--- Magic8Ball.py
+++ Magic8Ball.py
@@ -10,7 +10,6 @@ def main():
   #Create a list of your responses.
   print("Magic 8 Ball")
   #Prompt the user for their question.
-
   answers = ["As I see it, yes.", "As again later.", "Better not tell you now.", "Cannot predict now.", 
              "Concentrate and ask again.", "Don't count on it.", "It is certain.", "It is decidedly so.",
              "Most likely.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Outlook good.",
@@ -18,7 +17,6 @@ def main():
              "Yes - definitely.", "You may rely on it."]
   
   #Answer question randomly with one of the options from your earlier list.
-  
   num = random.random() #decimal 0 - 1
   num = num * 1000 #number 0 - 999 with decimals 
   num = int(num) #no more decimals
